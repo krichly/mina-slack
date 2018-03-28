20ce9fa6e47ec71f4793f71a6b391e75e6e42d89
Keven Richly <keven.richly@hpi.uni-potsdam.de>
2018-03-28 16:37:15 +0200

    ugly fix to load default stage settings if mina multistage is used

--- lib/mina/slack/tasks.rb
+++ lib/mina/slack/tasks.rb
@@ -15,6 +15,14 @@ namespace :slack do
 
       set(:last_commit, `git log -n 1 --pretty=format:"%H" origin/#{fetch(:branch)} --`)
       changes
+
+      _potential_stage = ARGV.first
+      if _stage_file_exists?(_potential_stage) && _argument_included_in_stages?(_potential_stage)
+        invoke _potential_stage
+      elsif _stage_file_exists?(_default_stage)
+        invoke _default_stage
+      end
+
       send_slack_message(slack_deploy_message, url)
     else
       print_status 'Unable to create Slack Announcement, no slack details provided.'
